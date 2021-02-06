def55838480930a7ea697de5c8fb715f88bdfa97
Steven G <stevenglab@gmail.com>
2021-02-05 20:09:31 -0700

    Updated DateTime style for formatted dates.

--- models/post.js
+++ models/post.js
@@ -1,10 +1,9 @@
 var mongoose = require('mongoose');
-var DateTime = require('luxon');
+var { DateTime } = require('luxon');
 
 var Schema = mongoose.Schema;
 
 var PostSchema = new Schema({
-    postNumber: { type: Number, required: true },
     postedBy: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     datePosted: { type: Date, required: true },
     body: { type: String, required: true, maxlength: 240 },
@@ -14,7 +13,7 @@ var PostSchema = new Schema({
 PostSchema
 .virtual('datePostedFormatted')
 .get(function() {
-    return DateTime.fromJSDate(this.purchase_date).toLocaleString(DateTime.DATE_MED)
+    return DateTime.fromJSDate(this.datePosted).toLocaleString(DateTime.DATETIME_MED)
 })
 
 
